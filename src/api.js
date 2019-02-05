918a34fedc56aca047895ee0cac73943a440bbac
maxkram <max.kramer@live.com>
2019-02-04 21:18:57 -0500

    with rethinDB

--- src/api.js
+++ src/api.js
@@ -1,11 +1,16 @@
 import openSocket from 'socket.io-client';
 const socket = openSocket('http://localhost:8000');
 
-function subscribeToTimer(cb) {
-  socket.on('timer', cb);
-  socket.emit('subscribeToTimer', 1000);
+function subscribeToDrawings(cb) {
+  socket.on('drawing', drawing => cb(drawing));
+  socket.emit('subscribeToDrawings');
+}
+
+function createDrawing(name) {
+  socket.emit('createDrawing', { name });
 }
 
 export {
-  subscribeToTimer,
+  createDrawing,
+  subscribeToDrawings,
 };
